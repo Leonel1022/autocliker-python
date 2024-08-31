6ff068111e7e4b21304ac7afe9368ac92eb32765
leonel1022 <130171328+Leonel1022@users.noreply.github.com>
2024-08-30 21:53:37 -0300

    Update autoclick.py

--- autoclick.py
+++ autoclick.py
@@ -1,15 +1,17 @@
 import pyautogui
 import time
 
-# Número de veces que presionará la tecla Espacio
+# Número de veces que presionará la tecla
 veces = 200
+tecla = a
 
 # Esperar 3 segundo antes de comenzar
 time.sleep(3)
 
 for _ in range(veces):
-    pyautogui.press('space')
-    # time.sleep(0.00001)  # Pausa pequeña entre cada pulsación para evitar posibles errores
+    pyautogui.press('tecla')
+    time.sleep(0.1)
+    # Pausa pequeña entre cada pulsación para evitar posibles errores
 
 # Salida después de completar
 print("Proceso completado.")
